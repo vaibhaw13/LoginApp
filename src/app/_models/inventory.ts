8c338930f6cf6a18004986acea8067b669c8c5a9
vaibhaw13 <vaibhawkumar13@gmail.com>
2020-02-13 12:02:37 +0530

    inventory model

---
+++ src/app/_models/inventory.ts
@@ -0,0 +1,11 @@
+export class Inventory {
+    _id : string;
+    productid: number;
+    productname: string;
+    vendor: string;
+    batchnum: number;
+    btachdate: Date;
+    mrp: Number;
+    quantity: Number;
+    status: String;
+}
